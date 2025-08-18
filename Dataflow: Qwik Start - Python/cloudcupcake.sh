209fcb0689e6a8e83a68c2e5c51a03d9c77d472e
Pothina Chandrikarani <140737438+ChanduCharanSample@users.noreply.github.com>
2025-08-19 00:05:19 +0530

    Update cloudcupcake.sh

--- Dataflow: Qwik Start - Python/cloudcupcake.sh
+++ Dataflow: Qwik Start - Python/cloudcupcake.sh
@@ -1,54 +1,53 @@
-#!/bin/bash
-
-YELLOW='\033[0;33m'
-NC='\033[0m'
+YELLOW='\033[0;33m' 
+NC='\033[0m' 
 
 pattern=(
 "**********************************************************"
-"**                 S U B S C R I B E  TO                **"
-"**                   CLOUDCUPCAKE-1217                  **"
-"**                                                      **"
+"**                 S U B S C R I B E TO                  **"
+"**                   cloudcupcake-1217                   **"
+"**                                                       **"
 "**********************************************************"
 )
 
-# Set region explicitly for lab
-gcloud config set compute/region us
-gsutil mb -l us gs://$DEVSHELL_PROJECT_ID-bucket/
+gcloud config set compute/region $LOCATION
 
-# Reset Dataflow API
+gsutil mb gs://$DEVSHELL_PROJECT_ID-bucket/
+
 gcloud services disable dataflow.googleapis.com
-sleep 40
+sleep 20
 gcloud services enable dataflow.googleapis.com
-sleep 40
+sleep 20
 
-# Run docker with variables
 docker run -it \
   -e DEVSHELL_PROJECT_ID=$DEVSHELL_PROJECT_ID \
-  -e LOCATION=us \
+  -e LOCATION=$LOCATION \
   python:3.9 /bin/bash -c '
     pip install "apache-beam[gcp]"==2.42.0 && \
     python -m apache_beam.examples.wordcount --output OUTPUT_FILE && \
-    BUCKET=gs://'$DEVSHELL_PROJECT_ID'-bucket && \
-    python -m apache_beam.examples.wordcount --project $DEVSHELL_PROJECT_ID \
+    HUSTLER=gs://$DEVSHELL_PROJECT_ID-bucket && \
+    python -m apache_beam.examples.wordcount \
+      --project $DEVSHELL_PROJECT_ID \
       --runner DataflowRunner \
-      --staging_location $BUCKET/staging \
-      --temp_location $BUCKET/temp \
-      --output $BUCKET/results/output \
-      --region us
+      --staging_location $HUSTLER/staging \
+      --temp_location $HUSTLER/temp \
+      --output $HUSTLER/results/output \
+      --region $LOCATION 
   '
 
-for line in "${pattern[@]}"; do
-    echo -e "${YELLOW}${line}${NC}"
+for line in "${pattern[@]}"
+do
+  echo -e "${YELLOW}${line}${NC}"
 done
 
 pattern=(
 "**********************************************************"
-"**                                                      **"
-"**            LAB COMPLETED SUCCESSFULLY!               **"
-"**                                                      **"
+"**                   S U B S C R I B E TO              **"
+"**                     cloudcupcake-1217               **"
+"**                                                     **"
 "**********************************************************"
 )
 
-for line in "${pattern[@]}"; do
-    echo -e "${YELLOW}${line}${NC}"
+for line in "${pattern[@]}"
+do
+  echo -e "${YELLOW}${line}${NC}"
 done
